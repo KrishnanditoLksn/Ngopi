7c9156a192d9b34e111c3ca0dd1c573a81f41320
KrishnanditoLksn <krislaksana123@gmail.com>
2025-02-23 12:03:06 +0700

    feat=category item component

---
+++ app/src/main/java/com/ditsdev/ngopi/CategoryItem.kt
@@ -0,0 +1,58 @@
+package com.ditsdev.ngopi
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ditsdev.ngopi.model.Category
+import com.ditsdev.ngopi.ui.theme.NgopiTheme
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(category.imageCategory),
+            contentDescription = null,
+            modifier = modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(category.textCategory),
+            fontSize = 10.sp,
+            modifier = modifier.paddingFromBaseline(16.dp, 8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryPreview() {
+    NgopiTheme {
+        CategoryItem(
+            category = Category(
+                R.drawable.icon_category_all,
+                R.string.category_cappuccino
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
